05f9c38b4effe33764d52d38851203464b650b61
Jangmoonki <mkjang0905@naver.com>
2019-07-26 11:36:05 +0900

    BOJ 15596번

---
+++ Backjoon/src/_15596.java
@@ -0,0 +1,17 @@
+
+public class _15596 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	public class Test{
+		public long sum(int[] a) {
+			long sum = 0;
+			for(int i = 0;i<a.length;i++) {
+				sum+=a[i];
+			}
+			return sum;
+		}
+	}
+}
